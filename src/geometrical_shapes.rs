fe7284ee2843be7800bd7bbdb465a1618168183b
afethi <fethiabderrahmane1@gmail.com>
2025-07-30 16:21:01 +0100

    final version

--- src/geometrical_shapes.rs
+++ src/geometrical_shapes.rs
@@ -14,7 +14,7 @@ pub trait Displayable {
     fn display(&mut self, x: i32, y: i32, color: Color) ;
 }
 
-#[derive(Debug, Clone)]
+#[derive(Debug, Clone, Copy)]
 pub struct Point {
     x: i32,
     y: i32,
@@ -78,8 +78,7 @@ impl Line {
 
 impl Drawable for Line {
    fn draw(&self, img: &mut Image) {
-        let mut rng = rand::rng();
-        let color = Color::rgb(rng.random(),rng.random(),rng.random());
+        let color = self.color();
         self.draw_with_color(img, color)
     }
 }
@@ -89,17 +88,16 @@ pub struct Triangle (Point, Point, Point);
 
 impl Triangle {
     pub fn new(a: &Point, b: &Point, c: &Point) -> Self {
-        Self(a.clone(), b.clone(), c.clone())
+        Self(*a, *b, *c)
     }
 }
 
 impl Drawable for Triangle {
     fn draw(&self, img: &mut Image) {
-        let mut rng = rand::rng();
-        let color = Color::rgb(rng.random(),rng.random(),rng.random());
-        let l1 = Line::new(self.0.clone(),self.1.clone());
-        let l2 = Line::new(self.1.clone(),self.2.clone());
-        let l3 = Line::new(self.2.clone(),self.0.clone());
+        let color = self.color();
+        let l1 = Line::new(self.0,self.1);
+        let l2 = Line::new(self.1,self.2);
+        let l3 = Line::new(self.2,self.0);
         l1.draw_with_color(img, color.clone());
         l2.draw_with_color(img, color.clone());
         l3.draw_with_color(img, color.clone());
@@ -107,17 +105,58 @@ impl Drawable for Triangle {
 }
 
 
-// #[derive(Debug)]
-// pub struct Circle (Point, Point);
+#[derive(Debug)]
+pub struct Circle {
+    center: Point,
+    radius: i32,
+}
 
-// impl Circle {
-//     pub fn random(width: i32, height: i32) -> Self {
-//          Self(Point::random(width, height), Point::random(width, height))
-//     }
-//     pub fn new(a: Point, b: Point) -> Self {
-//         Self(a, b)
-//     }
-// }
+impl Circle {
+    pub fn random(width: i32, height: i32) -> Self {
+        let center = Point::random(width, height);
+        let edge = Point::random(width, height);
+        let radius = ((edge.x - center.x).pow(2) + (edge.y - center.y).pow(2)) as f32;
+        Self::new(center, radius.sqrt() as i32)
+    }
+    pub fn new(a: Point, b: i32) -> Self {
+        Self {
+            center: a,
+            radius: b,
+        }
+    }
+}
+
+impl Drawable for Circle {
+    fn draw(&self, img : &mut Image){
+
+        let color = self.color();
+        let mut x = 0;
+        let cx = self.center.x ;
+        let cy = self.center.y ;
+        let mut y = - self.radius;
+        let mut p = - self.radius;
+        while x < -y {
+            if p > 0 {
+                y += 1;
+                p += 2*(x+y) +1
+            }else{
+                p+= 2*x + 1
+            }
+            img.display(cx + x , cy +y , color.clone());
+            img.display(cx - x , cy +y , color.clone());
+            img.display(cx + x , cy -y , color.clone());
+            img.display(cx - x , cy -y , color.clone());
+
+            img.display(cx + y , cy +x , color.clone());
+            img.display(cx + y , cy -x , color.clone());
+            img.display(cx - y , cy +x , color.clone());
+            img.display(cx - y , cy -x , color.clone());
+
+            x += 1;
+
+        }
+    }
+}
 
 #[derive(Debug)]
 pub struct Rectangle (Point, Point, Point, Point);
@@ -126,18 +165,17 @@ impl Rectangle {
     pub fn new(a: &Point, b: &Point) -> Self {
         let c = Point::new(a.x, b.y);
         let d = Point::new(b.x, a.y);
-        Self(a.clone(), b.clone(), c, d)
+        Self(*a, *b, c, d)
     }
 }
 
 impl Drawable for Rectangle {
     fn draw(&self, img : &mut Image){
-         let mut rng = rand::rng();
-        let color = Color::rgb(rng.random(),rng.random(),rng.random());
-        let l1 = Line::new(self.0.clone(),self.2.clone());
-        let l2 = Line::new(self.2.clone(),self.1.clone());
-        let l3 = Line::new(self.1.clone(),self.3.clone());
-        let l4 = Line::new(self.3.clone(),self.0.clone());
+        let color = self.color();
+        let l1 = Line::new(self.0,self.2);
+        let l2 = Line::new(self.2,self.1);
+        let l3 = Line::new(self.1,self.3);
+        let l4 = Line::new(self.3,self.0);
         l1.draw_with_color(img, color.clone());
         l2.draw_with_color(img, color.clone());
         l3.draw_with_color(img, color.clone());
